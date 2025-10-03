4fb7ac07031aa39416950f49ac1cae9dde27d531
AliYigitOzudogru <aliyigitozudogruu@gmail.com>
2025-10-03 11:43:12 +0300

    birleştirme aşaması devam ediyor

--- arda_random/Program.cs
+++ arda_random/Program.cs
@@ -6,11 +6,17 @@ class Program
     {
         Console.Clear();
         Console.CursorVisible = false;
-
+        int duvarBoyu1;
+        int duvarBoyu2;
         int width = 120;
         int height = 40;
         int blockWidth = 40;
-
+        bool isgamefinished= false;
+        int round = 1;
+        int duvarKonumux1;
+        int duvarKonumy1;
+        int duvarKonumux2;
+        int duvarKonumy2;
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
@@ -47,8 +53,42 @@ class Program
         Console.BackgroundColor = ConsoleColor.Red;     // arka plan kırmızı kalmalı
         Console.ForegroundColor = ConsoleColor.White;   // yazı beyaz olacak
         Console.Write("R");
-        Console.SetCursorPosition(0, 120);
 
+        while(!isgamefinished){
+            Random rnd = new Random();
+            int duvarBoyu1 = rnd.Next(3,7);
+            int duvarBoyu2 = rnd.Next(3,7);
+
+            int duvarKonumux1 = rnd.Next(40,81);// 1. Duvarın konumunun rastgele x pozisyonunu random bir şekilde oluşturuyor
+            int duvarKonumy1 = rnd.Next(0,121);//  1. Duvarın konumunun rastgele y pozisyonunu random bir şekilde oluşturuyor
+
+            int duvarKonumux2 = rnd.Next(40,81);// 2. Duvarın konumunun rastgele x pozisyonunu random bir şekilde oluşturuyor
+            int duvarKonumy2 = rnd.Next(0,121);//  2. Duvarın konumunun rastgele y pozisyonunu random bir şekilde oluşturuyor
+
+            for (int i = 0; i < duvarboyu1; i++)
+            {
+
+                Console.SetCursorPosition(duvarkonumx1, duvarkonumy1 + i);
+                Console.Write("#");
+            }
+
+            for (int a = 0; a < duvarboyu2; a++)
+            {
+
+
+                Console.SetCursorPosition(duvarkonumx2, duvarkonumy2 + a);
+                Console.Write("#");
+            }
+
+
+            if(round%3==0){
+
+
+            }
+            
+        }
+
+        Console.SetCursorPosition(0, 120);
 
         Console.ResetColor();
     }
